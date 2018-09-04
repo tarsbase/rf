d51e969e8af78d403c165ebff82ef0e815b23a85
Shaun Kerr <shaundavidkerr@gmail.com>
2018-09-04 14:20:35 +1200

    nix now pinned i think

--- rf.nix
+++ rf.nix
@@ -1,4 +1,11 @@
 let
+   inherit (import <nixpkgs> {}) fetchFromGitHub;
+   np = fetchFromGitHub {
+     owner = "NixOS";
+     repo = "nixpkgs";
+     rev = "d7c0d9a7f83b7f80e08888c040ea8a2ab7ca5f71";
+     sha256 = "0vb7ikjscrp2rw0dfw6pilxqpjm50l5qg2x2mn1vfh93dkl2aan7";
+   };
    config = {
       packageOverrides = pkgs: rec {
          haskellPackages = pkgs.haskellPackages.override {
@@ -9,7 +16,7 @@ let
          };
       };
    };
-   pkgs = import <nixpkgs> { inherit config; };
+   pkgs = import np { inherit config; };
 in
    rec {
       inherit pkgs;
